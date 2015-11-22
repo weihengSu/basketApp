73f7c3eddc6ab9a3c110a29d24689325eae059ed
weiheng <weihengsu3@gmail.com>
2015-11-22 10:56:51 -0500

    modified interface

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main")
 def main():
-   return "Welcome!"
+   return render_template('index.html')
+ 
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
    
 if __name__ =="__main__":
    app.run()
