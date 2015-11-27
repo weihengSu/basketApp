32282555f150d40c98452e59ed2e8a2df824a9fa
weiheng <weihengsu3@gmail.com>
2015-11-26 19:46:19 -0500

    modified login/logout

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from models import User
 
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
+#login_manager = LoginManager()
+#login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-	return User.get(user_id)
+#@login_manager.user_loader
+#def load_user(user_id):
+#	return User.get(user_id)
 
 @app.route("/")
 def main():
@@ -20,22 +20,32 @@ def main():
 def showSignUp():
 	return render_template('signup.html')
 
-@app.route('/signUp',methods=['POST'])
+@app.route('/signUp',methods=['GET','POST'])
 def signUp():
 	if request.method == 'POST':
-		username = request.form['inputName']
-		email = request.form['inputEmail']
-		password = request.form['inputPassword']
-		if len(username) > 0:
-			create_user(username, email, password)
-	return render_template('/')	
+		conn = psycopg2.connect(database="basketball", user="postgres", password="password")							 
+		cur = conn.cursor()
+		username = request.form['username']
+		email = request.form['email']
+		password = request.form['password']
+		#if len(username)>0:
+		#	create_user(username, email, password)
+		#conn = psycopg2.connect(database="basketball", user="postgres", password="password")							 
+		#cur = conn.cursor()		
+		cur.execute("INSERT INTO basket_user (user_id, user_email,user_password) VALUES (%s, %s, %s)",(username, email, password))
+		conn.commit()
+		cur.close()
+		conn.close()
+		return redirect(url_for('main'))
+	else:
+		return render_template("signup.html")
 
 
 @app.route('/showSignin')
 def showSignin():
-    return render_template('signin.html')
+	return render_template('signin.html')
 	
-#@app.route('/', methods=['GET','POST'])
+
 #def login():
 #	if request.method == 'POST':
 #		username = request.form['username']
@@ -47,9 +57,43 @@ def showSignin():
 		#else:
 		#	return redirect(url_for('login'))
 #	return render_template('/login')
+@app.route('/login', methods=['GET','POST'])
+def login():
+	try: 
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		conn = psycopg2.connect("dbname='basketball' user='postgres' "
+									 "host='localhost' password='password'")
+		cur = conn.cursor()
+		cur.execute("SELECT user_id FROM basket_user WHERE user_id = %(id)s", {'id': username })		
+		result = cur.fetchall()
+		if len(result) > 0:
+			cur.execute("SELECT user_email FROM basket_user WHERE user_id = %(id)s", {'id': username }) 
+			result_email = cur.fetchall()
+			cur.execute("SELECT user_password FROM basket_user WHERE user_id = %(id)s", {'id': username }) 
+			result_password = cur.fetchall()
+			
+			if (email == result_email[0][0] and password == result_password[0][0]):
+				return redirect(url_for('userHome'))
+			else:
+				return render_template('error.html',error = 'Wrong Email address or Password.')
+		else:
+			return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+	except Exception as e:
+		return render_template('error.html',error = str(e))
+	finally:
+		cur.close()
+		conn.close()	
+		
 
+@app.route('/userHome')
+def userHome():
+	return render_template('userHome.html')		
+		
+		
 	
-		
-		
 if __name__ =="__main__":
    app.run(debug=True)
--- login_check.py
+++ login_check.py
@@ -1,38 +1,11 @@
 import psycopg2
 
 def create_user(username, email, password):
-	conn = psycopg2.connect("dbname='basketball' user='postgres' "
-									 "host='localhost' password='password'")							 
-	cur = conn.cursor()
-	cur.execute("SELECT user_id FROM basket_users \n"
-				"WHERE id = '" + username + "';")
-	results = cur.fetchall() 
-	if(username not in results):			
-		cur.execute("INSERT INTO basket_user(user_id, user_email, user_password) VALUES ('" + username + "', '"+ email + "', '" + password+ "');")
-		conn.commit()
-		return render_template('/')
-	else:
-		return render_template('/showSignUp')
+	conn = psycopg2.connect(database="basketball", user="postgres", password="password")							 
+	cur = conn.cursor()		
+	#cur.execute("INSERT INTO basket_user(user_id, user_email, user_password) VALUES ('" + username + "', '"+ email + "', '" + password+ "');")
+	cur.execute("INSERT INTO basket_user (user_id, user_email,user_password) VALUES (%s, %s, %s)",(username, email, password))
+	conn.commit()
 	cur.close()
 	conn.close()
-	
-def check_login(username, email, password):
-	conn = psycopg2.connect("dbname='basketball' user='postgres' "
-									 "host='localhost' password='password'")
-	cur = conn.cursor()
-
-	cur.execute("SELECT user_id FROM basket_users \n"
-				"WHERE id = '" + username + "';")
-	results = cur.fetchall() 
-	if (len(results) > 0):
-		cur.execute("SELECT user_email, user_password FROM basket_users \n"
-					  "WHERE id = '" + username + "';")
-		result2 = cur.fetchall()
-		if (email == result2[0] and password == result2[1]):
-			return render_template()
-	cur.close()
-	conn.close()
-
-	# Check entered credentials against database entry
-	if len(results) == 0:
-		print("Found no results.")
+	return render_template('/showSignUp')
